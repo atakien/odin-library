3443f473944c353c2f7c29d5f1091bc6562668f4
Atakan Akgun <47303791+atakien@users.noreply.github.com>
2024-09-14 20:41:29 +0300

    Added HTML content and styles

---
+++ script.js
@@ -0,0 +1,16 @@
+const btnEl = document.querySelector("button");
+const title = document.querySelector(".title");
+
+btnEl.addEventListener("click", () => {
+    title.classList.toggle("toggle");
+})
+
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
