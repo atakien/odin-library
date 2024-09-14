d8ad86db90126275e5865ce1939690add5f81a7f
Atakan Akgun <47303791+atakien@users.noreply.github.com>
2024-09-15 00:07:04 +0300

    Added first 4 functionality

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const libraryContainer = document.querySelector(".library-container");
 
 const myLibrary = [];
 
-// Event listener for form submission
 formEl.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    // Get values from the form
     const bookTitleEl = document.querySelector(".title-input").value;
     const authorEl = document.querySelector(".author-input").value;
     const pageEl = document.querySelector(".page-input").value;
@@ -15,18 +13,15 @@ formEl.addEventListener("submit", (e) => {
 
     const readStatus = radioEl ? radioEl.id : "Not specified";
 
-    // Create a new book object
     const bookInfo = new Book(bookTitleEl, authorEl, pageEl, readStatus);
     myLibrary.push(bookInfo);
     formEl.reset();
     
-    // Add the new book to the library
     addBookToLibrary(bookInfo);
     
     console.log(myLibrary);
 });
 
-// Constructor function for Book
 function Book(title, author, page, read) {
     this.title = title;
     this.author = author;
@@ -34,9 +29,7 @@ function Book(title, author, page, read) {
     this.read = read;
 }
 
-// Function to add a book card to the library
 function addBookToLibrary(book) {
-    // Define the template for a book card
     const cardTemplate = `
         <div class="book-card">
             <div><span>Title:</span> ${book.title}</div>
@@ -46,10 +39,8 @@ function addBookToLibrary(book) {
             <button class="close-button">+</button>
         </div>`;
     
-    // Create a new div element and set its innerHTML
     const cardElement = document.createElement('div');
     cardElement.innerHTML = cardTemplate;
     
-    // Append the new card element to the library container
     libraryContainer.appendChild(cardElement);
 }
